f0912d25778a9d92aa5572e808506e8f21fafbd4
Bodzio0019 <nikolas.ed@onet.eu>
2024-03-08 15:29:29 +0100

    Created aside, with dropdown category options in JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const asideCategory1 = document.querySelector(".aside-category-1");
+const asideCategory2 = document.querySelector(".aside-category-2");
+
+asideCategory1.addEventListener("click", () => {
+    document.querySelector(".option-group-1").classList.toggle("aside-hide-1");
+    document.querySelector(".category-arrow-1").classList.toggle("category-arrow-turn-1");
+});
+asideCategory2.addEventListener("click", () => {
+    document.querySelector(".option-group-2").classList.toggle("aside-hide-2");
+    document.querySelector(".category-arrow-2").classList.toggle("category-arrow-turn-2");
+});
