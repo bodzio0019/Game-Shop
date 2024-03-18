55ca65363bf6a737e8dc3ad1fe38621e819c1bbc
Bodzio0019 <nikolas.ed@onet.eu>
2024-03-18 19:09:34 +0100

    Created data folder with data.js file, moved item page to folder item-pages and created JS showing items on index.html

--- script/index.js
+++ script/index.js
@@ -1,6 +1,30 @@
+import { data } from "../data/data.js";
+
+// On start
+renderItems();
+
+// Aside
 document.querySelectorAll(".aside-category").forEach( (btn) => {
     btn.addEventListener("click", function() {
         this.nextElementSibling.classList.toggle("aside-hide");
         this.querySelector(".category-arrow").classList.toggle("category-arrow-turn");
     });
 });
+
+// Items
+function renderItems() {
+    let html = ``;
+    data.forEach( (data) => {
+        html +=`<a href="item-pages/${data.page}.html">
+        <div class="game-item">
+        <img src="img/${data.image}.jpg" alt="Game picture" />
+        <p class="game-title">${data.name}</p>
+        <p class="game-price">${data.zloty},${data.grosze} zł</p>
+        </div>
+        </a>`
+    });
+    
+    (function display() {
+        document.querySelector(".items").innerHTML = html;
+    })();
+};
