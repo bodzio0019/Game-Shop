0e52291382c9fd5a1ded88e9d4d7d9c91b2c76e0
Bodzio0019 <nikolas.ed@onet.eu>
2024-03-21 13:35:04 +0100

    Added more games to data.js, so there are 14 now, created them pages and img

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 export const data = [{
         pathname: "/item-pages/god-of-war.html",
         page: "god-of-war",
-        image: "god",
+        image: "godOfWar",
         name: "God of War",
         developer: "Sony Interactive Entertainment Europe",
         status: 1,
@@ -10,11 +10,11 @@ export const data = [{
         description: `<p>Living as a man outside the shadow of the gods, Kratos must adapt to unfamiliar lands, unexpected threats and a second chance at being a father. Together with his son Atreus, the pair will venture into the brutal realm of Midgard and fight to fulfil a deeply personal quest.</p><br />
         <ul>
             <li>Journey to a dark, elemental world of fearsome creatures – straight from the pages of Norse mythology.</li>
-            <li>Conquer your foes in vicious close-quarters combat that rewards tactical thinking and lethal precision.
+            <li>Conquer your foes in vicious close-quarters combat that rewards tactical thinking and lethal precision.</li>
             <li>Discover an emotionally gripping tale of father and son, as Kratos is made to control the rage that has long defined him.</li>
         </ul>`,
         platform: ["PS4"],
-        release: "4/10/2029",
+        release: "4/10/2019",
         genre: "Action",
     },{
         pathname: "/item-pages/diablo-4.html",
@@ -52,4 +52,186 @@ export const data = [{
         platform: ["PS4", "PS5"],
         release: "19/11/2021",
         genre: "FPS",
+    },{
+        pathname: "/item-pages/uncharted-collection.html",
+        page: "uncharted-collection",
+        image: "unchartedCollection",
+        name: "Uncharted: The Nathan Drake Collection",
+        developer: "Sony Interactive Entertainment Europe",
+        status: 1,
+        price: 8900,
+        discount: 0,
+        description: `<p>Experience one of the most revered game series of all time as you follow the perilous journey of Nathan Drake across the globe, from humble beginnings to extraordinary discoveries.</p><br />
+        <p>Meet an unforgettable cast of characters as Drake puts life and friendship on the line in a race against ruthless enemies to uncover unimaginable treasures.</p><br />
+        <p>Uncharted: The Nathan Drake Collection includes the single-player campaigns only for Uncharted: Drake’s Fortune, Uncharted 2: Among Thieves, and Uncharted 3: Drake’s Deception.</p><br />
+        <p>You will also gain access to the multiplayer beta for the upcoming Uncharted 4: A Thief’s End, available from 4 – 13 December. You can find more information on downloading the beta from unchartedthegame.com.</p><br />`,
+        platform: ["PS4"],
+        release: "7/10/2015",
+        genre: "Action",
+    },{
+        pathname: "/item-pages/demon's-souls.html",
+        page: "demon's-souls",
+        image: "demon'sSouls",
+        name: "Demon's Souls",
+        developer: "Sony Interactive Entertainment Europe",
+        status: 1,
+        price: 33900,
+        discount: 0,
+        description: `<p>From PlayStation Studios and Bluepoint Games comes a remake of the PlayStation classic, Demon's Souls. Entirely rebuilt from the ground up and masterfully enhanced, this remake introduces the horrors of a fog-laden, dark fantasy land to a whole new generation of gamers. Those who've faced its trials and tribulations before can once again challenge the darkness in stunning visual quality and incredible performance.</p><br />
+        <p>In his quest for power, the 12th King of Boletaria, King Allant, channelled the ancient Soul Arts, awakening a demon from the dawn of time itself, The Old One. With the summoning of The Old One, a colourless fog swept across the land, unleashing nightmarish creatures that hungered for human souls. Those whose souls were stripped from them lost their minds — left only with the desire to attack the sane that remained.</p><br />
+        <p>Now, Boletaria is cut off from the outside world, and the knights who dare penetrate the deep fog to free the land from its plight are never seen again. As a lone warrior who has braved the baneful fog, you must face the hardest of challenges to earn the title "Slayer of Demons" and send The Old One back to its slumber.</p><br />`,
+        platform: ["PS5"],
+        release: "12/11/2020",
+        genre: "RPG",
+    },{
+        pathname: "/item-pages/returnal.html",
+        page: "returnal",
+        image: "returnal",
+        name: "Returnal",
+        developer: "Sony Interactive Entertainment Europe",
+        status: 1,
+        price: 33900,
+        discount: 0,
+        description: `<p>BREAK THE CYCLE</p><br />
+        <p>After crash-landing on a shape-shifting alien planet, Selene finds herself fighting tooth and nail for survival. Again and again she’s defeated, forced to restart her journey every time she dies.</p><br />
+        <p>In this roguelike shooter, both the planet and your equipment change with every cycle, forcing you to adapt your play style and take on evolving challenges.</p><br />
+        <p>Choose to go solo through the shifting labyrinth of Atropos, or share your entire journey with another player in online Co-op Mode. Then, when you’re ready, take on the Tower of Sisyphus – an ascending gauntlet of increasingly dangerous phases focused on putting your combat skills to the test.</p><br />
+        <p>Engage enemies in bullet hell-fuelled clashes. Scavenge alien tech for upgrades to your abilities. Forge a personal connection with the planet and piece together Selene’s story.</p><br />
+        <p>PS5 IN-GAME FEATURES:</p><br />
+        <ul>
+            <li>Fast loading: near-instant loading speeds of the PS5 console get you rapidly back into the action.</li>
+            <li>Tempest 3D AudioTech: hear multidirectional 3D Audio – bullets flying past you, enemies circling overhead, or unseen foes creeping up behind you.</li>
+            <li>Adaptive triggers: switch between firing modes with a single adaptive trigger – go from aiming down sights to your gun’s alternative fire.</li>
+            <li>Haptic feedback: sense in-game actions – while exploring fragments of Selene’s memories, or when firing visceral alien weapons.</li>
+        </ul>`,
+        platform: ["PS5"],
+        release: "30/4/2021",
+        genre: "FPS",
+    },{
+        pathname: "/item-pages/marvel's-spider-man.html",
+        page: "marvel's-spider-man",
+        image: "marvel'sSpiderMan",
+        name: "Marvel’s Spider-Man: Game of the Year Edition",
+        developer: "Sony Interactive Entertainment Europe",
+        status: 1,
+        price: 21900,
+        discount: 0,
+        description: `<p>Upgrade to the Game of the Year Edition and play the story chapters in The City That Never Sleeps – featuring additional missions and challenges, brand new factions of enemies from the Spider-Man universe, and more suits to unlock.</p><br />
+        <p>BE GREATER.</p><br />
+        <p>Explore a new chapter in Marvel’s Spider-Man universe as a more experienced Peter Parker.</p><br />
+        <p>Starring one of the world’s most iconic Super Heroes, Marvel's Spider-Man features the acrobatic abilities, improvisation and web-slinging that the wall-crawler is famous for, while also introducing elements never-before-seen in a Spider-Man game.</p><br />
+        <p>From traversing by parkour and unique environmental interactions, to new combat and blockbuster action, it’s Spider-Man unlike any you’ve played before.</p><br />`,
+        platform: ["PS4"],
+        release: "28/8/2019",
+        genre: "Action",
+    },{
+        pathname: "/item-pages/assassin's-creed-origins.html",
+        page: "assassin's-creed-origins",
+        image: "assassin'sCreedOrigins",
+        name: "Assassin's Creed Origins",
+        developer: "UBISOFT ENTERTAINMENT SA",
+        status: 1,
+        price: 29900,
+        discount: 0,
+        description: `<p>ASSASSIN’S CREED ORIGINS IS A NEW BEGINNING</p><br />
+        <p>Ancient Egypt, a land of majesty and intrigue, is disappearing in a ruthless fight for power. Unveil dark secrets and forgotten myths as you go back to the one founding moment: The Origins of the Assassin’s Brotherhood.</p><br />
+        <p>Sail down the Nile, uncover the mysteries of the pyramids or fight your way against dangerous ancient factions and wild beasts as you explore this gigantic and unpredictable land.</p><br />
+        <p>Engage into multiple quests and gripping stories as you cross paths with strong and memorable characters, from the wealthiest high-born to the most desperate outcasts.</p><br />
+        <p>Experience a completely new way to fight. Loot and use dozens of weapons with different characteristics and rarities. Explore deep progression mechanics and challenge your skills against unique and powerful bosses.</p><br />`,
+        platform: ["PS4"],
+        release: "27/10/2017",
+        genre: "RPG",
+    },{
+        pathname: "/item-pages/call-of-duty-modern-warfare.html",
+        page: "call-of-duty-modern-warfare",
+        image: "callOfDutyModerWarfare",
+        name: "Call of Duty: Modern Warfare",
+        developer: "Activision Blizzard Int'l BV",
+        status: 1,
+        price: 29900,
+        discount: 0,
+        description: `<p>Includes:</p><br />
+        <ul>
+            <li>Call of Duty: Modern Warfare.</li>
+            <li>In-game XRK Weapons Pack.</li>
+            <li>In-game Tactical Knife.</li>
+        </ul>
+        <p>Experience a visceral Campaign or assemble your team in the ultimate online playground with multiple Special Ops challenges and a mix of Multiplayer maps and modes.</p><br />`,
+        platform: ["PS4"],
+        release: "25/10/2019",
+        genre: "FPS",
+    },{
+        pathname: "/item-pages/ratchet-clank-rift-apart.html",
+        page: "ratchet-clank-rift-apart",
+        image: "ratchetClankRiftApart",
+        name: "Ratchet & Clank: Rift Apart",
+        developer: "Sony Interactive Entertainment",
+        status: 1,
+        price: 33900,
+        discount: 0,
+        description: `<p>Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality. Jump between action-packed worlds, and beyond at mind-blowing speeds – complete with dazzling visuals and an insane arsenal – as the intergalactic adventurers blast onto the PS5 console.</p>`,
+        platform: ["PS5"],
+        release: "11/6/2021",
+        genre: "Action",
+    },{
+        pathname: "/item-pages/elden-ring.html",
+        page: "elden-ring",
+        image: "eldenRing",
+        name: "Elden Ring",
+        developer: "BANDAI NAMCO Entertainment Inc.",
+        status: 1,
+        price: 25900,
+        discount: 0,
+        description: `<p>The Golden Order has been broken.</p><br />
+        <p>In the Lands Between ruled by Queen Marika the Eternal, the Elden Ring, the source of the Erdtree, has been shattered.</p><br />
+        <p>Marika's offspring, demigods all, claimed the shards of the Elden Ring known as the Great Runes, and the mad taint of their newfound strength triggered a war: The Shattering. A war that meant abandonment by the Greater Will.</p><br />
+        <p>And now the guidance of grace will be brought to the Tarnished who were spurned by the grace of gold and exiled from the Lands Between.</p><br />
+        <p>Ye dead who yet live, your grace long lost, follow the path to the Lands Between beyond the foggy sea to stand before the Elden Ring.</p><br />
+        <p>And become the Elden Lord.</p><br />`,
+        platform: ["PS4","PS5"],
+        release: "25/2/2022",
+        genre: "RPG",
+    },{
+        pathname: "/item-pages/cyberpunk-2077.html",
+        page: "cyberpunk-2077",
+        image: "cyberpunk2077",
+        name: "Cyberpunk 2077",
+        developer: "CD Projekt",
+        status: 1,
+        price: 20900,
+        discount: 0,
+        description: `<p>Cyberpunk 2077 is an open-world action-adventure from the creators of The Witcher 3: Wild Hunt, CD Projekt Red.</p><br />
+        <p>Set in Night City, a megalopolis obsessed with power, glamour and body modification, you play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality.</p><br />
+        <p>Upgraded with next-gen in mind and featuring free additional content, customize your character and playstyle as you take on jobs, build a reputation, and unlock upgrades.</p><br />
+        <p>The relationships you forge and the choices you make will shape the story and the world around you. Legends are made here. What will yours be?</p><br />`,
+        platform: ["PS4","PS5"],
+        release: "15/2/2022",
+        genre: "FPS",
+    },{
+        pathname: "/item-pages/grand-theft-auto-V.html",
+        page: "grand-theft-auto-V",
+        image: "grandTheftAutoV",
+        name: "Grand Theft Auto V",
+        developer: "Rockstar Games",
+        status: 1,
+        price: 16900,
+        discount: 0,
+        description: `<p>Experience blockbuster hits Grand Theft Auto V and GTA Online - now upgraded for PlayStation 5 with stunning visuals, faster loading, adaptive triggers, haptic feedback, Tempest 3D audio and more, plus exclusive content for GTA Online players.</p><br />`,
+        platform: ["PS4"],
+        release: "15/3/2022",
+        genre: "Action",
+    },{
+        pathname: "/item-pages/the-witcher-3-wild-hunt.html",
+        page: "the-witcher-3-wild-hunt",
+        image: "theWitcher3WildHunt",
+        name: "The Witcher 3: Wild Hunt",
+        developer: "CD Projekt",
+        status: 1,
+        price: 12900,
+        discount: 0,
+        description: `<p>You are Geralt, from the city of Rivia. All around you the towns and settlements of the Northern Kingdoms are being razed to the ground, as an otherworldly invading army known only as the Wild Hunt leaves a trail of blood-soaked destruction in its wake.</p><br />
+        <p>As you prepare for a thunderous confrontation with the Wild Hunt, you’ll uncover a complex, gripping story and meet unforgettable characters. While exploring the Northern Kingdoms, you’ll discover that mysteries lurk inside every village, tree, and shadow.</p><br />`,
+        platform: ["PS4","PS5"],
+        release: "14/12/2022",
+        genre: "RPG",
     }];
