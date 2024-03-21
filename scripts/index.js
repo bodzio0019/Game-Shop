99ccc2ccb91ae79197add58a036e33bfa8dcdc01
Bodzio0019 <nikolas.ed@onet.eu>
2024-03-22 00:14:50 +0100

    Created all sort features on all navbar pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { data } from "../data/data.js";
 
 // On start
-renderItems();
+renderItems(data);
 
 // Aside
 document.querySelectorAll(".aside-category").forEach( (btn) => {
@@ -11,8 +11,63 @@ document.querySelectorAll(".aside-category").forEach( (btn) => {
     });
 });
 
+// Sort
+const sortedData = structuredClone(data);
+const select = document.querySelector("#sort");
+select.addEventListener("change", () => {
+    switch(select.value) {
+        case "bestsellers":
+            renderItems(sortedData);
+            break;
+        case "lowest price":
+            data.sort((a, b) => { 
+                let first = "";
+                let second = "";
+                if(a.discount !== 0) {
+                    first = a.discount;
+                } else {
+                    first = a.price;
+                };
+                if(b.discount !== 0) {
+                    second = b.discount;
+                } else {
+                    second = b.price;
+                };
+               return first - second;
+            });
+            renderItems(data);
+            break;
+        case "highest price":
+            data.sort((a, b) => { 
+                let first = "";
+                let second = "";
+                if(a.discount !== 0) {
+                    first = a.discount;
+                } else {
+                    first = a.price;
+                };
+                if(b.discount !== 0) {
+                    second = b.discount;
+                } else {
+                    second = b.price;
+                };
+               return second - first;
+            });
+            renderItems(data);
+            break;
+        case "name a z":
+            data.sort((a, b) => a.name.localeCompare(b.name));
+            renderItems(data);
+            break;
+        case "name z a":
+            data.sort((a, b) => b.name.localeCompare(a.name));
+            renderItems(data);
+            break;
+    };
+});
+
 // Items
-function renderItems() {
+function renderItems(data) {
     let html = ``;
     data.forEach( (data) => {
         if (data.discount === 0) {
--- scripts/ps5.js
+++ scripts/ps5.js
@@ -1,10 +1,10 @@
-import { data } from "../data/data.js";
+import { data as data1 } from "../data/data.js";
 
 // On start
-const deals = data.filter((item) => {
+const data = data1.filter((item) => {
     return (item.platform[0] === "PS5")||(item.platform[1] === "PS5");
 });
-renderItems();
+renderItems(data);
 
 // Aside
 document.querySelectorAll(".aside-category").forEach( (btn) => {
@@ -14,10 +14,65 @@ document.querySelectorAll(".aside-category").forEach( (btn) => {
     });
 });
 
+// Sort
+const sortedData = structuredClone(data);
+const select = document.querySelector("#sort");
+select.addEventListener("change", () => {
+    switch(select.value) {
+        case "bestsellers":
+            renderItems(sortedData);
+            break;
+        case "lowest price":
+            data.sort((a, b) => { 
+                let first = "";
+                let second = "";
+                if(a.discount !== 0) {
+                    first = a.discount;
+                } else {
+                    first = a.price;
+                };
+                if(b.discount !== 0) {
+                    second = b.discount;
+                } else {
+                    second = b.price;
+                };
+               return first - second;
+            });
+            renderItems(data);
+            break;
+        case "highest price":
+            data.sort((a, b) => { 
+                let first = "";
+                let second = "";
+                if(a.discount !== 0) {
+                    first = a.discount;
+                } else {
+                    first = a.price;
+                };
+                if(b.discount !== 0) {
+                    second = b.discount;
+                } else {
+                    second = b.price;
+                };
+               return second - first;
+            });
+            renderItems(data);
+            break;
+        case "name a z":
+            data.sort((a, b) => a.name.localeCompare(b.name));
+            renderItems(data);
+            break;
+        case "name z a":
+            data.sort((a, b) => b.name.localeCompare(a.name));
+            renderItems(data);
+            break;
+    };
+});
+
 // Items
-function renderItems() {
+function renderItems(data) {
     let html = ``;
-    deals.forEach( (data) => {
+    data.forEach( (data) => {
         if (data.discount === 0) {
             html +=`<a href="item-pages/${data.page}.html">
             <div class="game-item">
