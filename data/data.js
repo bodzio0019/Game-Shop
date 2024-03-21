73d52a6b90355e99ac94b7388436344aef59b948
Bodzio0019 <nikolas.ed@onet.eu>
2024-03-21 10:41:58 +0100

    Added 2 more games to data.js, created status and platforms displaying on item page

--- data/data.js
+++ data/data.js
@@ -4,29 +4,52 @@ export const data = [{
         image: "god",
         name: "God of War",
         developer: "Sony Interactive Entertainment Europe",
-        status: '<p class="status"><i class="fa-solid fa-check" style="color: #048e20;"></i>Available</p>',
+        status: 1,
         price: 24999,
+        discount: 0,
         description: `<p>Living as a man outside the shadow of the gods, Kratos must adapt to unfamiliar lands, unexpected threats and a second chance at being a father. Together with his son Atreus, the pair will venture into the brutal realm of Midgard and fight to fulfil a deeply personal quest.</p><br />
         <ul>
             <li>Journey to a dark, elemental world of fearsome creatures – straight from the pages of Norse mythology.</li>
             <li>Conquer your foes in vicious close-quarters combat that rewards tactical thinking and lethal precision.
             <li>Discover an emotionally gripping tale of father and son, as Kratos is made to control the rage that has long defined him.</li>
         </ul>`,
-        platform: "PS4",
+        platform: ["PS4"],
         release: "4/10/2029",
-        publisher: "Sony Interactive Entertainment Europe",
         genre: "Action",
     },{
         pathname: "/item-pages/diablo-4.html",
         page: "diablo-4",
-        image: "god",
+        image: "diablo4",
         name: "Diablo IV - Standard Edition",
-        developer: "Blizzard entertaiment",
-        status: '<p class="status"><i class="fa-solid fa-check" style="color: #048e20;"></i>Available</p>',
+        developer: "Blizzard entertaiment, Inc.",
+        status: 1,
         price: 34900,
-        description: ``,
-        platform: "ps4",
-        release: "",
-        publisher: "",
-        genre: "",
+        discount: 0,
+        description: `<p>Diablo IV - Standard Edition includes:</p><br />
+        <ul>
+            <li>Diablo IV for PS4 / PS5</li>
+            <li>Inarius Wings & Inarius Murloc Pet in Diablo III</li>
+            <li>Amalgam of Rage Mount in World of Warcraft</li>
+        </ul><br />
+        <p>Diablo IV is the next-gen action RPG experience with endless evil to slaughter, countless abilities to master, nightmarish Dungeons, and legendary loot. Embark on the campaign solo or with friends, meeting memorable characters through beautifully dark settings and a gripping story, or go rogue through an expansive End Game and shared world where players will meet in towns to trade, team up to battle World Bosses, or descend into PVP zones to test their skills against other players - no lobbies necessary - with cross-play and cross-progression on all available platforms.</p><br />
+        <p>This is only the beginning for Diablo IV, with new events, stories, seasons, rewards, and more looming on the horizon.</p>`,
+        platform: ["PS4", "PS5"],
+        release: "5/6/2023",
+        genre: "RPG",
+    },{
+        pathname: "/item-pages/battlefield-2042.html",
+        page: "battlefield-2042",
+        image: "battlefield2042",
+        name: "Battlefield 2042",
+        developer: "EA Swiss Sarl",
+        status: 1,
+        price: 34990,
+        discount: 0,
+        description: `<p>Battlefield 2042 is a first-person shooter that marks the return to the iconic all-out warfare of the franchise. With the help of a cutting-edge arsenal, engage in intense, immersive multiplayer battles.</p><br />
+        <p>It’s all or nothing in Battlefield 2042 – Season 7: Turning Point. Do whatever it takes in Season 7: Turning Point, which brings the battle for Earth’s most valuable resource to the Atacama Desert in Chile. There’s no holding back for your squad as you engage in lawless suburban combat on the new map Haven and revisit a returning fan-favorite front: Stadium. Use destruction as your ally and give the enemy everything you’ve got with new gear like the SCZ-3 SMG, the Predator SRAW gadget, and the XFAD-4 Draugr aerial bomber. Unlock 100 new tiers of Battle Pass content in a battle for ultimate power.</p><br />
+        <p>Lead your team to victory in both large all-out warfare and close-quarters combat on maps from the world of 2042 and classic Battlefield titles. Find your playstyle in class-based gameplay and take on several experiences comprising elevated versions of Conquest and Breakthrough. Explore Battlefield Portal, a platform where players can discover, create, and share unexpected battles from Battlefield's past and present.</p><br />
+        <p>This game includes optional in-game purchases of virtual currency that can be used to acquire virtual in-game items.</p>`,
+        platform: ["PS4", "PS5"],
+        release: "19/11/2021",
+        genre: "FPS",
     }];
--- scripts/item.js
+++ scripts/item.js
@@ -15,7 +15,7 @@ function renderItem() {
     <div class="item-cart">
     <h2>${item.name}</h2>
     <p class="developer">${item.developer}</p>
-    <p class="status">${item.status}
+    <p class="status"><i class="fa-solid fa-check" style="color: #048e20;"></i>Available</p>
     <p class="price">${(item.price/100).toFixed(2)} zł</p>
     <div class="cart-wrapper">
         <input type="number" class="add-quantity" />
@@ -23,7 +23,7 @@ function renderItem() {
     </div>
     <p class="small-info"><i class="fa-solid fa-truck" style="color: #000000;"></i>Free shipping from 300 zł.</p>
     <p class="small-info"><i class="fa-solid fa-star" style="color: #000000;"></i></i>We are an authorized seller and guarantee the originality of the products offered.</p>
-    <p class="small-info"><i class="fa-solid fa-rotate-left" style="color: #000000;"></i>Return is possible within 14 days from the date of receipt of the goods</p>
+    <p class="small-info"><i class="fa-solid fa-rotate-left" style="color: #000000;"></i>Return is possible within 14 days from the date of receipt of the goods.</p>
     </div>`;
     const htmlInfo = ` <div class="item-info-wrapper">
     <h2 class="game-description active-option">Description</h2>
@@ -34,7 +34,7 @@ function renderItem() {
     </div>
     <div class="game-details-text">
     <div class="game-details-wrapper">
-        <p>Platform:</p><p>${item.platform}</p>
+        <p>Platform:</p><p class="js-selector">${item.platform[0]}</p>
         <p>Release:</p><p>${item.release}</p>
         <p>Publisher:</p><p>${item.developer}</p>
         <p>Genre:</p><p>${item.genre}</p>
@@ -42,6 +42,16 @@ function renderItem() {
     </div>`;
     document.querySelector(".item-main-header").innerHTML = htmlHeader;
     document.querySelector(".item-main-info").innerHTML = htmlInfo;
+
+    if (item.status === 0) {
+        document.querySelector(".status").innerHTML = `<p class="status"><i class="fa-solid fa-xmark" style="color: #e32400;"></i>Out of stock</p>`;
+        document.querySelector(".add-quantity").disabled = true;
+        document.querySelector(".add-to-cart").disabled = true;
+    };
+
+    if(item.platform[1]) {
+        document.querySelector(".js-selector").innerHTML = `${item.platform[0]}, ${item.platform[1]}`;
+    };
 };
 
 // Item info
